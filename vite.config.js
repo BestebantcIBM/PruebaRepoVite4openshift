2f49cf134dfcf1e44950c7704d2ff6d12443afd4
Bryan Esteban Torres Cruz <Bryan.Esteban.Torres@ibm.com>
2024-02-29 10:51:03 -0500

    changes2

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 8080,
-   },
-  
-   server: {
-    port: 8080,
-    host: true,
-    origin: "http://0.0.0.0:8080",
-   },
-  
+
  
 })
