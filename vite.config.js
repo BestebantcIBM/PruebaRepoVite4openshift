5e9c701188347583ffc3662fd931f743908155fc
Bryan Esteban Torres Cruz <Bryan.Esteban.Torres@ibm.com>
2024-02-29 13:55:51 -0500

    cambio

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+   server: {
+    port: 8080,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+   },
+  
  
 })
