270cc67089c5d1b44fc5ca815ef6c3094577d6f0
Bryan Esteban Torres Cruz <Bryan.Esteban.Torres@ibm.com>
2024-02-29 14:31:59 -0500

    upload

--- Dockerfile
+++ Dockerfile
@@ -30,10 +30,10 @@ USER 1001
 ENV NODE_ENV development
 
 # Listen on port 3000
-ENV PORT 3000
+ENV PORT 8080
 
 # Container exposes port 3000
-EXPOSE 3000
+EXPOSE 8080
 
 # Start node process
 CMD ["npm", "run", "dev"]
--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-   server: {
-    port: 8080,
-    host: true,
-    origin: "http://0.0.0.0:8080",
-   },
   
  
 })
